9ff989dbe3834a9e3462c82fb16b7c90aac48691
xR4YM0ND <xr4ym0nd@gmail.com>
2026-01-04 00:05:55 +0100

    added more ores

--- src/main/java/net/agedaddition/init/BlockInit.java
+++ src/main/java/net/agedaddition/init/BlockInit.java
@@ -355,11 +355,142 @@ public class BlockInit {
         ItemGroups.NATURAL
     );
 
+    public static final Block MARLSTONE_EMERALD_ORE = register("marlstone_emerald_ore",
+        new ExperienceDroppingBlock(
+            UniformIntProvider.create(0, 1),
+            AbstractBlock.Settings.create().mapColor(MapColor.STONE_GRAY).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(3.0F, 3.0F)
+        ),
+        true,
+        ItemGroups.NATURAL
+    );
 
+    public static final Block ALPINE_GNEISS_EMERALD_ORE = register("alpine_gneiss_emerald_ore",
+        new ExperienceDroppingBlock(
+            UniformIntProvider.create(0, 1),
+            AbstractBlock.Settings.create().mapColor(MapColor.STONE_GRAY).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(3.0F, 3.0F)
+        ),
+        true,
+        ItemGroups.NATURAL
+    );
 
+    public static final Block LATERIT_EMERALD_ORE = register("laterit_emerald_ore",
+        new ExperienceDroppingBlock(
+            UniformIntProvider.create(0, 1),
+            AbstractBlock.Settings.create().mapColor(MapColor.STONE_GRAY).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(3.0F, 3.0F)
+        ),
+        true,
+        ItemGroups.NATURAL
+    );
 
+    public static final Block GRANITE_EMERALD_ORE = register("granite_emerald_ore",
+        new ExperienceDroppingBlock(
+            UniformIntProvider.create(0, 1),
+            AbstractBlock.Settings.create().mapColor(MapColor.STONE_GRAY).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(3.0F, 3.0F)
+        ),
+        true,
+        ItemGroups.NATURAL
+    );
 
+    public static final Block TRAVERTIN_EMERALD_ORE = register("travertin_emerald_ore",
+        new ExperienceDroppingBlock(
+            UniformIntProvider.create(0, 1),
+            AbstractBlock.Settings.create().mapColor(MapColor.STONE_GRAY).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(3.0F, 3.0F)
+        ),
+        true,
+        ItemGroups.NATURAL
+    );
 
+    public static final Block TUFF_EMERALD_ORE = register("tuff_emerald_ore",
+        new ExperienceDroppingBlock(
+            UniformIntProvider.create(0, 1),
+            AbstractBlock.Settings.create().mapColor(MapColor.STONE_GRAY).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(3.0F, 3.0F)
+        ),
+        true,
+        ItemGroups.NATURAL
+    );
+
+    public static final Block GROUTLESS_RUBBLESTONE_EMERALD_ORE = register("groutless__rubblestone_emerald_ore",
+        new ExperienceDroppingBlock(
+            UniformIntProvider.create(0, 1),
+            AbstractBlock.Settings.create().mapColor(MapColor.STONE_GRAY).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(3.0F, 3.0F)
+        ),
+        true,
+        ItemGroups.NATURAL
+    );
+
+    public static final Block SMOOTH_BASALT_EMERALD_ORE = register("smooth_basalt_emerald_ore",
+        new ExperienceDroppingBlock(
+            UniformIntProvider.create(0, 1),
+            AbstractBlock.Settings.create().mapColor(MapColor.STONE_GRAY).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(3.0F, 3.0F)
+        ),
+        true,
+        ItemGroups.NATURAL
+    );
+
+    public static final Block BLACKSTONE_EMERALD_ORE = register("blackstone_emerald_ore",
+        new ExperienceDroppingBlock(
+            UniformIntProvider.create(0, 1),
+            AbstractBlock.Settings.create().mapColor(MapColor.STONE_GRAY).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(3.0F, 3.0F)
+        ),
+        true,
+        ItemGroups.NATURAL
+    );
+
+    // LAPIS
+
+    public static final Block TRAVERTINE_LAPIS_ORE = register("travertine_lapis_ore",
+        new ExperienceDroppingBlock(
+            UniformIntProvider.create(0, 1),
+            AbstractBlock.Settings.create().mapColor(MapColor.STONE_GRAY).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(3.0F, 3.0F)
+        ),
+        true,
+        ItemGroups.NATURAL
+    );
+
+    public static final Block MARLSTONE_LAPIS_ORE = register("marlstone_lapis_ore",
+        new ExperienceDroppingBlock(
+            UniformIntProvider.create(0, 1),
+            AbstractBlock.Settings.create().mapColor(MapColor.STONE_GRAY).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(3.0F, 3.0F)
+        ),
+        true,
+        ItemGroups.NATURAL
+    );
+
+    public static final Block ALPINE_GNEISS_LAPIS_ORE = register("alpine_gneiss_lapis_ore",
+        new ExperienceDroppingBlock(
+            UniformIntProvider.create(0, 1),
+            AbstractBlock.Settings.create().mapColor(MapColor.STONE_GRAY).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(3.0F, 3.0F)
+        ),
+        true,
+        ItemGroups.NATURAL
+    );
+
+    public static final Block GROUTLESS_RUBBLESTONE_LAPIS_ORE = register("groutless_rubblestone_lapis_ore",
+        new ExperienceDroppingBlock(
+            UniformIntProvider.create(0, 1),
+            AbstractBlock.Settings.create().mapColor(MapColor.STONE_GRAY).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(3.0F, 3.0F)
+        ),
+        true,
+        ItemGroups.NATURAL
+    );
+
+    public static final Block SMOOTH_BASALT_LAPIS_ORE = register("smooth_basalt_lapis_ore",
+        new ExperienceDroppingBlock(
+            UniformIntProvider.create(0, 1),
+            AbstractBlock.Settings.create().mapColor(MapColor.STONE_GRAY).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(3.0F, 3.0F)
+        ),
+        true,
+        ItemGroups.NATURAL
+    );
+
+    public static final Block BLACKSTONE_LAPIS_ORE = register("blackstone_lapis_ore",
+        new ExperienceDroppingBlock(
+            UniformIntProvider.create(0, 1),
+            AbstractBlock.Settings.create().mapColor(MapColor.STONE_GRAY).instrument(NoteBlockInstrument.BASEDRUM).requiresTool().strength(3.0F, 3.0F)
+        ),
+        true,
+        ItemGroups.NATURAL
+    );
 
 
 
