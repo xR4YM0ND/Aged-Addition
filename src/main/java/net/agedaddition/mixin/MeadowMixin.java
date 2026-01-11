1eea8c29e9161c4309b46f85ee8163ba2aa0842c
xR4YM0ND <xr4ym0nd@gmail.com>
2026-01-11 20:47:09 +0100

    added all ore block strengths

--- src/main/java/net/agedaddition/mixin/MeadowMixin.java
+++ src/main/java/net/agedaddition/mixin/MeadowMixin.java
@@ -2,15 +2,21 @@ package net.agedaddition.mixin;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyArgs;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 
+import net.agedaddition.block.CustomRedstoneOreBlock;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.block.MapColor;
+import net.minecraft.block.RedstoneOreBlock;
 import net.minecraft.block.enums.NoteBlockInstrument;
-
+import net.minecraft.util.math.intprovider.IntProvider;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.satisfy.meadow.core.registry.ObjectRegistry;
 
 @Mixin(ObjectRegistry.class)
@@ -29,4 +35,176 @@ public class MeadowMixin {
                .requiresTool()
                .strength(10.0F, 100.0F));
     }
+
+    // Limestone Salt
+    @WrapOperation(
+        method = "lambda$static$0",
+        at = @At(value = "NEW", target = "Lnet/minecraft/block/ExperienceDroppingBlock;"),
+        remap = false
+    )
+    private static ExperienceDroppingBlock modifyLimestoneSalt(IntProvider experienceDropped, AbstractBlock.Settings settings, Operation<ExperienceDroppingBlock> original) {
+
+        IntProvider customExperience = UniformIntProvider.create(1, 3);
+
+        AbstractBlock.Settings customSettings = AbstractBlock.Settings.create()
+            .mapColor(MapColor.STONE_GRAY)
+            .instrument(NoteBlockInstrument.BASEDRUM)
+            .requiresTool()
+            .strength(14.0F, 100.0F);
+
+        return original.call(customExperience, customSettings);
+    }
+
+    // Limestone Coal
+    @WrapOperation(
+        method = "lambda$static$1",
+        at = @At(value = "NEW", target = "Lnet/minecraft/block/ExperienceDroppingBlock;"),
+        remap = false
+    )
+    private static ExperienceDroppingBlock modifyLimestoneCoal(IntProvider experienceDropped, AbstractBlock.Settings settings, Operation<ExperienceDroppingBlock> original) {
+
+        IntProvider customExperience = UniformIntProvider.create(1, 3);
+
+        AbstractBlock.Settings customSettings = AbstractBlock.Settings.create()
+            .mapColor(MapColor.STONE_GRAY)
+            .instrument(NoteBlockInstrument.BASEDRUM)
+            .requiresTool()
+            .strength(14.0F, 100.0F);
+
+        return original.call(customExperience, customSettings);
+    }
+
+    // Limestone Lapis
+    @WrapOperation(
+        method = "lambda$static$2",
+        at = @At(value = "NEW", target = "Lnet/minecraft/block/ExperienceDroppingBlock;"),
+        remap = false
+    )
+    private static ExperienceDroppingBlock modifyLimestoneLapis(IntProvider experienceDropped, AbstractBlock.Settings settings, Operation<ExperienceDroppingBlock> original) {
+
+        IntProvider customExperience = UniformIntProvider.create(1, 3);
+
+        AbstractBlock.Settings customSettings = AbstractBlock.Settings.create()
+            .mapColor(MapColor.STONE_GRAY)
+            .instrument(NoteBlockInstrument.BASEDRUM)
+            .requiresTool()
+            .strength(14.0F, 100.0F);
+
+        return original.call(customExperience, customSettings);
+    }
+
+    // Limestone Gold
+    @WrapOperation(
+        method = "lambda$static$3",
+        at = @At(value = "NEW", target = "Lnet/minecraft/block/ExperienceDroppingBlock;"),
+        remap = false
+    )
+    private static ExperienceDroppingBlock modifyLimestoneGold(IntProvider experienceDropped, AbstractBlock.Settings settings, Operation<ExperienceDroppingBlock> original) {
+
+        IntProvider customExperience = UniformIntProvider.create(1, 3);
+
+        AbstractBlock.Settings customSettings = AbstractBlock.Settings.create()
+            .mapColor(MapColor.STONE_GRAY)
+            .instrument(NoteBlockInstrument.BASEDRUM)
+            .requiresTool()
+            .strength(14.0F, 100.0F);
+
+        return original.call(customExperience, customSettings);
+    }
+
+    // Limestone Emerald
+    @WrapOperation(
+        method = "lambda$static$4",
+        at = @At(value = "NEW", target = "Lnet/minecraft/block/ExperienceDroppingBlock;"),
+        remap = false
+    )
+    private static ExperienceDroppingBlock modifyLimestoneEmerald(IntProvider experienceDropped, AbstractBlock.Settings settings, Operation<ExperienceDroppingBlock> original) {
+
+        IntProvider customExperience = UniformIntProvider.create(1, 3);
+
+        AbstractBlock.Settings customSettings = AbstractBlock.Settings.create()
+            .mapColor(MapColor.STONE_GRAY)
+            .instrument(NoteBlockInstrument.BASEDRUM)
+            .requiresTool()
+            .strength(14.0F, 100.0F);
+
+        return original.call(customExperience, customSettings);
+    }
+
+    // Limestone Iron
+    @WrapOperation(
+        method = "lambda$static$5",
+        at = @At(value = "NEW", target = "Lnet/minecraft/block/ExperienceDroppingBlock;"),
+        remap = false
+    )
+    private static ExperienceDroppingBlock modifyLimestoneIron(IntProvider experienceDropped, AbstractBlock.Settings settings, Operation<ExperienceDroppingBlock> original) {
+
+        IntProvider customExperience = UniformIntProvider.create(1, 3);
+
+        AbstractBlock.Settings customSettings = AbstractBlock.Settings.create()
+            .mapColor(MapColor.STONE_GRAY)
+            .instrument(NoteBlockInstrument.BASEDRUM)
+            .requiresTool()
+            .strength(14.0F, 100.0F);
+
+        return original.call(customExperience, customSettings);
+    }
+
+    // Limestone Copper
+    @WrapOperation(
+        method = "lambda$static$6",
+        at = @At(value = "NEW", target = "Lnet/minecraft/block/ExperienceDroppingBlock;"),
+        remap = false
+    )
+    private static ExperienceDroppingBlock modifyLimestoneCopper(IntProvider experienceDropped, AbstractBlock.Settings settings, Operation<ExperienceDroppingBlock> original) {
+
+        IntProvider customExperience = UniformIntProvider.create(1, 3);
+
+        AbstractBlock.Settings customSettings = AbstractBlock.Settings.create()
+            .mapColor(MapColor.STONE_GRAY)
+            .instrument(NoteBlockInstrument.BASEDRUM)
+            .requiresTool()
+            .strength(14.0F, 100.0F);
+
+        return original.call(customExperience, customSettings);
+    }
+
+    // Limestone Diamond
+    @WrapOperation(
+        method = "lambda$static$7",
+        at = @At(value = "NEW", target = "Lnet/minecraft/block/ExperienceDroppingBlock;"),
+        remap = false
+    )
+    private static ExperienceDroppingBlock modifyLimestoneDiamond(IntProvider experienceDropped, AbstractBlock.Settings settings, Operation<ExperienceDroppingBlock> original) {
+
+        IntProvider customExperience = UniformIntProvider.create(1, 3);
+
+        AbstractBlock.Settings customSettings = AbstractBlock.Settings.create()
+            .mapColor(MapColor.STONE_GRAY)
+            .instrument(NoteBlockInstrument.BASEDRUM)
+            .requiresTool()
+            .strength(14.0F, 100.0F);
+
+        return original.call(customExperience, customSettings);
+    }
+
+    // Limestone Redstone
+    @WrapOperation(
+        method = "lambda$static$8",
+        at = @At(value = "NEW", target = "Lnet/minecraft/block/ExperienceDroppingBlock;"),
+        remap = false
+    )
+    private static ExperienceDroppingBlock modifyLimestoneRedstone(IntProvider experienceDropped, AbstractBlock.Settings settings, Operation<ExperienceDroppingBlock> original) {
+
+        IntProvider customExperience = UniformIntProvider.create(1, 3);
+
+        AbstractBlock.Settings customSettings = AbstractBlock.Settings.create()
+            .mapColor(MapColor.STONE_GRAY)
+            .instrument(NoteBlockInstrument.BASEDRUM)
+            .requiresTool()
+            .strength(14.0F, 100.0F);
+
+        return original.call(customExperience, customSettings);
+    }
+
 }
---
+++ src/main/java/net/agedaddition/mixin/StalMixin.java
@@ -0,0 +1,130 @@
+package net.agedaddition.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.agedaddition.block.ExperienceDroppingPillarBlock;
+
+import net.minecraft.block.AbstractBlock;
+import net.minecraft.block.ExperienceDroppingBlock;
+import net.minecraft.sound.BlockSoundGroup;
+import net.minecraft.util.math.intprovider.IntProvider;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import net.stal.alloys.block.StalAlloysBlocks;
+
+@Mixin(StalAlloysBlocks.class)
+public class StalMixin {
+
+    // Deepslate Nickel
+    @WrapOperation(method = "<clinit>", at = @At(value = "NEW", target = "Lnet/minecraft/block/ExperienceDroppingBlock;", ordinal = 1, remap = false))
+    private static ExperienceDroppingBlock modifyDeepslateNickel(IntProvider experienceDropped, AbstractBlock.Settings settings, Operation<ExperienceDroppingBlock> original) {
+        IntProvider customExperience = UniformIntProvider.create(1, 3);
+        AbstractBlock.Settings customSettings = AbstractBlock.Settings.create()
+                .sounds(BlockSoundGroup.DEEPSLATE)
+                .requiresTool()
+                .strength(92.0F, 800.0F);
+        return new ExperienceDroppingPillarBlock(customExperience, customSettings);
+    }
+
+    // Deepslate Chromium
+    @WrapOperation(method = "<clinit>", at = @At(value = "NEW", target = "Lnet/minecraft/block/ExperienceDroppingBlock;", ordinal = 4, remap = false))
+    private static ExperienceDroppingBlock modifyDeepslateChromium(IntProvider experienceDropped, AbstractBlock.Settings settings, Operation<ExperienceDroppingBlock> original) {
+        IntProvider customExperience = UniformIntProvider.create(1, 3);
+        AbstractBlock.Settings customSettings = AbstractBlock.Settings.create()
+                .sounds(BlockSoundGroup.DEEPSLATE)
+                .requiresTool()
+                .strength(92.0F, 800.0F);
+        return new ExperienceDroppingPillarBlock(customExperience, customSettings);
+    }
+
+    // Deepslate Carbon
+    @WrapOperation(method = "<clinit>", at = @At(value = "NEW", target = "Lnet/minecraft/block/ExperienceDroppingBlock;", ordinal = 7, remap = false))
+    private static ExperienceDroppingBlock modifyDeepslateCarbon(IntProvider experienceDropped, AbstractBlock.Settings settings, Operation<ExperienceDroppingBlock> original) {
+        IntProvider customExperience = UniformIntProvider.create(1, 3);
+        AbstractBlock.Settings customSettings = AbstractBlock.Settings.create()
+                .sounds(BlockSoundGroup.DEEPSLATE)
+                .requiresTool()
+                .strength(92.0F, 800.0F);
+        return new ExperienceDroppingPillarBlock(customExperience, customSettings);
+    }
+
+    // Deepslate Zinc
+    @WrapOperation(method = "<clinit>", at = @At(value = "NEW", target = "Lnet/minecraft/block/ExperienceDroppingBlock;", ordinal = 9, remap = false))
+    private static ExperienceDroppingBlock modifyDeepslateZinc(IntProvider experienceDropped, AbstractBlock.Settings settings, Operation<ExperienceDroppingBlock> original) {
+        IntProvider customExperience = UniformIntProvider.create(1, 3);
+        AbstractBlock.Settings customSettings = AbstractBlock.Settings.create()
+                .sounds(BlockSoundGroup.DEEPSLATE)
+                .requiresTool()
+                .strength(92.0F, 800.0F);
+        return new ExperienceDroppingPillarBlock(customExperience, customSettings);
+    }
+
+    // Deepslate Tin
+    @WrapOperation(method = "<clinit>", at = @At(value = "NEW", target = "Lnet/minecraft/block/ExperienceDroppingBlock;", ordinal = 11, remap = false))
+    private static ExperienceDroppingBlock modifyDeepslateTin(IntProvider experienceDropped, AbstractBlock.Settings settings, Operation<ExperienceDroppingBlock> original) {
+        IntProvider customExperience = UniformIntProvider.create(1, 3);
+        AbstractBlock.Settings customSettings = AbstractBlock.Settings.create()
+                .sounds(BlockSoundGroup.DEEPSLATE)
+                .requiresTool()
+                .strength(92.0F, 800.0F);
+        return new ExperienceDroppingPillarBlock(customExperience, customSettings);
+    }
+
+    // Nickel
+    @WrapOperation(method = "<clinit>", at = @At(value = "NEW", target = "Lnet/minecraft/block/ExperienceDroppingBlock;", ordinal = 0, remap = false))
+    private static ExperienceDroppingBlock modifyNickel(IntProvider experienceDropped, AbstractBlock.Settings settings, Operation<ExperienceDroppingBlock> original) {
+        IntProvider customExperience = UniformIntProvider.create(1, 3);
+        AbstractBlock.Settings customSettings = AbstractBlock.Settings.create()
+                .sounds(BlockSoundGroup.STONE)
+                .requiresTool()
+                .strength(8.0F, 50.0F);
+        return new ExperienceDroppingBlock(customExperience, customSettings);
+    }
+
+    // Chromium
+    @WrapOperation(method = "<clinit>", at = @At(value = "NEW", target = "Lnet/minecraft/block/ExperienceDroppingBlock;", ordinal = 3, remap = false))
+    private static ExperienceDroppingBlock modifyChromium(IntProvider experienceDropped, AbstractBlock.Settings settings, Operation<ExperienceDroppingBlock> original) {
+        IntProvider customExperience = UniformIntProvider.create(1, 3);
+        AbstractBlock.Settings customSettings = AbstractBlock.Settings.create()
+                .sounds(BlockSoundGroup.STONE)
+                .requiresTool()
+                .strength(8.0F, 50.0F);
+        return new ExperienceDroppingBlock(customExperience, customSettings);
+    }
+
+    // Carbon
+    @WrapOperation(method = "<clinit>", at = @At(value = "NEW", target = "Lnet/minecraft/block/ExperienceDroppingBlock;", ordinal = 6, remap = false))
+    private static ExperienceDroppingBlock modifyCarbon(IntProvider experienceDropped, AbstractBlock.Settings settings, Operation<ExperienceDroppingBlock> original) {
+        IntProvider customExperience = UniformIntProvider.create(1, 3);
+        AbstractBlock.Settings customSettings = AbstractBlock.Settings.create()
+                .sounds(BlockSoundGroup.STONE)
+                .requiresTool()
+                .strength(8.0F, 50.0F);
+        return new ExperienceDroppingBlock(customExperience, customSettings);
+    }
+
+    // Zinc
+    @WrapOperation(method = "<clinit>", at = @At(value = "NEW", target = "Lnet/minecraft/block/ExperienceDroppingBlock;", ordinal = 8, remap = false))
+    private static ExperienceDroppingBlock modifyZinc(IntProvider experienceDropped, AbstractBlock.Settings settings, Operation<ExperienceDroppingBlock> original) {
+        IntProvider customExperience = UniformIntProvider.create(1, 3);
+        AbstractBlock.Settings customSettings = AbstractBlock.Settings.create()
+                .sounds(BlockSoundGroup.STONE)
+                .requiresTool()
+                .strength(8.0F, 50.0F);
+        return new ExperienceDroppingBlock(customExperience, customSettings);
+    }
+
+    // Tin
+    @WrapOperation(method = "<clinit>", at = @At(value = "NEW", target = "Lnet/minecraft/block/ExperienceDroppingBlock;", ordinal = 10, remap = false))
+    private static ExperienceDroppingBlock modifyTin(IntProvider experienceDropped, AbstractBlock.Settings settings, Operation<ExperienceDroppingBlock> original) {
+        IntProvider customExperience = UniformIntProvider.create(1, 3);
+        AbstractBlock.Settings customSettings = AbstractBlock.Settings.create()
+                .sounds(BlockSoundGroup.STONE)
+                .requiresTool()
+                .strength(8.0F, 50.0F);
+        return new ExperienceDroppingBlock(customExperience, customSettings);
+    }
+
+}
