bd8ee3d92c826b474bbaaac7f6baf1d99fce6cff
xR4YM0ND <xr4ym0nd@gmail.com>
2026-03-15 09:55:17 +0100

    bump

--- src/main/java/net/agedaddition/AgedAdditionMain.java
+++ src/main/java/net/agedaddition/AgedAdditionMain.java
@@ -5,7 +5,9 @@ import net.agedaddition.init.ConfigInit;
 import net.agedaddition.init.ItemInit;
 import net.agedaddition.init.TagInit;
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.registry.FuelRegistry;
 import net.minecraft.util.Identifier;
+import net.stal.alloys.item.StalAlloysItems;
 
 public class AgedAdditionMain implements ModInitializer {
 
@@ -18,6 +20,7 @@ public class AgedAdditionMain implements ModInitializer {
         ConfigInit.init();
         TagInit.init();
         //BiomeInit.init();
+        FuelRegistry.INSTANCE.add(StalAlloysItems.CARBON_PLATE, 24000);
     }
 
     public static Identifier identifierOf(String name) {
